0bbec289cadc103bc928a31830abeb32f5ca6ccb
CTpeJLok <good-i-evil@yandex.ru>
2024-09-24 14:27:41 +0300

    create user_manager app

--- app/core/settings.py
+++ app/core/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user_manager.apps.UserManagerConfig",
 ]
 
 MIDDLEWARE = [
