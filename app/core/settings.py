1b557a9eb1d6576c4f05dd40f4ca48eef3cb514a
CTpeJLok <good-i-evil@yandex.ru>
2024-09-24 15:01:52 +0300

    create disk app with main page

--- app/core/settings.py
+++ app/core/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user_manager.apps.UserManagerConfig",
+    "disk_manager.apps.DiskManagerConfig",
 ]
 
 MIDDLEWARE = [
