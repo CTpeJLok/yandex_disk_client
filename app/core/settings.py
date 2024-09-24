199400277dcb34de1a652a1e62b507b0c697e60c
CTpeJLok <good-i-evil@yandex.ru>
2024-09-24 17:32:50 +0300

    add cache system

--- app/core/settings.py
+++ app/core/settings.py
@@ -88,3 +88,13 @@ USE_TZ = False
 STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ["REDIS_URL"],
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
